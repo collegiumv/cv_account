1943fb517b8ea0802962f97b32a46285044de7d1
Michael Aldridge <aldridge.mac@gmail.com>
2015-07-14 00:43:16 -0500

    change log level in validate code

--- validate.py
+++ validate.py
@@ -3,20 +3,20 @@ import logging, re
 class Validate:
     def __init__(self, config):
         self.loungeACL = config["ACL"]
-    
+        self.logger.getLogger("validation")
+
     def netID(self, netID):
         if netID in self.loungeACL:
-            logging.debug("Valid netID %s", netID)
+            self.logger.debug("Valid netID %s", netID)
             return True
         else:
-            logging.warning("Invalid netID %s", netID)
+            self.logger.debug("Invalid netID %s", netID)
             return False
 
     def user(self, user):
         uValid = re.compile(ur'^[A-Za-z0-9.-_]{1,30}$')
         if re.search(uValid, user):
             logging.debug("User %s passed regex check", user)
-            # Do the ldap search to make sure they didn't pick a username
             return True
         else:
             logging.debug("User %s failed regex check", user)
